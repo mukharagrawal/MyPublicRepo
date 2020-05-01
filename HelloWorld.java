1e0d8ecfcb7933bc00f202cd95f07a82f0fbff02
Mukhar Agrawal <64415144+mukharagrawal@users.noreply.github.com>
2020-05-01 16:08:37 +0530

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-        System. out. println("Hello Java git changes");
+        System. out. println("Hello Java git change 2");
     }
 }
