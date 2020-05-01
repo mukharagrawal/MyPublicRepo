47c1c68fb0e013218f992b1037d1fd9505e80852
Mukhar Agrawal <64415144+mukharagrawal@users.noreply.github.com>
2020-05-01 17:42:12 +0530

    Commit Changes 7

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-        System. out. println("Hello Java git change 6");
+        System. out. println("Hello Java git change 7");
     }
 }
